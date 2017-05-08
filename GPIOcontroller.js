cdae51d1b8a4fd80df54e1ea44ec0738d708647d
Marcos Estévez <marcos.stvz@gmail.com>
2017-05-08 13:33:35 +0000

    Door opens

---
+++ GPIOcontroller.js
@@ -0,0 +1,21 @@
+var gpio = require("onoff").Gpio;
+var led = new gpio(18, 'out');
+var isOpened = false; 
+
+var unlockRelay = function (cb) {
+
+	if (!isOpened) {
+		led.writeSync(1);
+		isOpened = true;
+		console.log('In gpio-write: open');
+		setTimeout(function () {
+			led.writeSync(0);
+			isOpened = false;
+			console.log('In gpio-write: close');
+			cb();
+		}, 3000);
+	}
+
+}
+
+module.exports = { unlockRelay: unlockRelay };
---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var GPIOCtrl = require('./GPIOcontroller.js');
+
+app.get('/unlock/', function(req, res) { 
+	GPIOCtrl.unlockRelay(function () {
+		res.send('OK');
+	});
+ });
+
+app.listen(3000); 
+
+console.log('App Server running at port 3000');
